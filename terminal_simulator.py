d8b38b7a4edcb2b2f03440ec7f0ad96ab1fced85
Timur <timur@shakirov.com.ua>
2023-12-14 10:38:38 +0100

    heroku deployment

--- terminal_simulator.py
+++ terminal_simulator.py
@@ -4,6 +4,7 @@ import threading
 from datetime import datetime
 from time import sleep
 import requests
+import os
 
 
 app = Flask(__name__)
@@ -92,4 +93,5 @@ def process():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
